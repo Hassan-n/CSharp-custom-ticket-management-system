7c32154998e961e4aabf0e6a8d0a83b39407411c
Hassan-n <141544246+Hassan-n@users.noreply.github.com>
2023-09-10 12:05:34 +0200

    Add files via upload

---
+++ CustomTicketManagementSystem/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Net.Sockets;
+
+namespace CustomTicketManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialize the data structure to store tickets
+            Queue<Ticket> tickets = new Queue<Ticket>();
+
+            // Start processing student requests
+            while (true)
+            {
+                Console.WriteLine("Enter student name or 'exit' to quit:");
+                string name = Console.ReadLine();
+
+                if (name.ToLower() == "exit")
+                {
+                    break; // Exit the loop if the user enters 'exit'
+                }
+
+                Console.WriteLine("Enter service required:");
+                string service = Console.ReadLine();
+
+                // Generate a unique ticket number for the student
+                int ticketNumber = tickets.Count + 1;
+
+                // Create a new ticket object and add it to the data structure
+                Ticket newTicket = new Ticket(ticketNumber, name, service);
+                tickets.Enqueue(newTicket);
+
+                Console.WriteLine($"Ticket {ticketNumber} created for {name} ({service})");
+            }
+
+            // Process resolved tickets
+            while (tickets.Count > 0)
+            {
+                // Retrieve the ticket at the front of the data structure without removing it
+                Ticket currentTicket = tickets.Peek();
+                Console.WriteLine($"Processing ticket {currentTicket.TicketNumber} for {currentTicket.Name} ({currentTicket.Service})");
+
+                // Remove the ticket from the data structure
+                tickets.Dequeue();
+                Console.WriteLine($"Ticket {currentTicket.TicketNumber} resolved for {currentTicket.Name} ({currentTicket.Service})");
+            }
+        }
+    }
+}
+
+ 
---
+++ CustomTicketManagementSystem/Ticket.cs
@@ -0,0 +1,27 @@
+﻿using CustomTicketManagementSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CustomTicketManagementSystem
+{
+    internal class Ticket
+    {
+        public int TicketNumber { get; set; }
+        public string Name { get; set; }
+        public string Service { get; set; }
+
+        public Ticket(int ticketNumber, string name, string service)
+        {
+            TicketNumber = ticketNumber;
+            Name = name;
+            Service = service;
+        }
+    }
+}
+
+
+
